ae20e13a1bdec4c2697ac2d352166cc1659f49fe
John Schaub <schaubjs@gmail.com>
2021-12-04 11:41:24 -0700

    css styling and new pokemon pics

---
+++ src/Pokegame.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Pokegame.css'
+
+
+class Pokegame extends Component {
+  
+  render() { 
+    return (
+      <div>
+        <h1>Pokegame!</h1>
+      </div>
+     );
+  }
+}
+ 
+export default Pokegame;
